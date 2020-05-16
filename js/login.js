0342c56b1e5a6ad918c936100f01bcdabfc0175c
Giovani Faleiros <agokdroid@gmail.com>
2020-05-16 09:35:02 -0300

    Ajustes no JavaScript

--- js/login.js
+++ js/login.js
@@ -5,6 +5,6 @@ function login(){
     if(usuario != "admin@admin" && senha != "1234"){
         alert("Usuário ou senha incorreto!");
     }else{
-        window.location.href= "html/index.html";
+        window.location.href= "/html/index.html";
     }
 }
