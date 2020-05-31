9b1007c4af99d85044d8b2e6ca7b78fd7815e36c
Giovani Faleiros <agokdroid@gmail.com>
2020-05-31 17:35:31 -0300

    Update tabela.js

--- js/tabela.js
+++ js/tabela.js
@@ -197,14 +197,12 @@ function geraTabela() {
     tabela.setAttribute("border", "2");
     tabela.setAttribute("align", "center");
 
-    let rotulos = elExclusivos.toString();
     let chartGraph = new Chart(ctx, {
-        type: 'line',
+        type: 'doughnut',
         data: {
-            labels: rotulos,
+            labels: elExclusivos,
             datasets: [{
-                label: ["Jan","Fev","Mar","Abr","Mai","Jun","Jul","Ago","Set","Out","Nov","Dez"],
-                data: [5, 10, 5, 14, 20, 15, 6, 14, 8, 12, 15, 5, 10],
+                data: ocorrencias,
                 borderWidth: 6,
                 borderColor: 'rgba(77,166,253,0.85)',
                 backgroundColor: 'transparent',
