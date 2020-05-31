287a95b95e0dd59de7479e16ff06e8a046ba830e
Giovani Faleiros <agokdroid@gmail.com>
2020-05-31 19:26:00 -0300

    Update tabela.js

--- js/tabela.js
+++ js/tabela.js
@@ -27,13 +27,13 @@ function geraTabela() {
 
     div.innerHTML = "";
 
-    function sortFunction(a, b){
+    function sortFunction(a, b) {
         return (a - b);
     }
 
     for (let elemento of document.getElementById("elementos").value.split(";")) {
         elementos.push(elemento.trim());
-        if(! isNaN(elemento)){
+        if (!isNaN(elemento)) {
             soma = soma + Number(elemento)
         }
     }
@@ -44,23 +44,23 @@ function geraTabela() {
 
     ocorrencias = elementos.reduce(function (obj, item) {
         obj[item] = (obj[item] || 0) + 1;
-        if(obj[item] > qtdmoda){
+        if (obj[item] > qtdmoda) {
             qtdmoda = obj[item];
             moda = item;
         }
         return obj;
     }, {});
-    
-    if(isNaN(elementos[1])){
+
+    if (isNaN(elementos[1])) {
         tipovariavel = "Qualitativa"
-    }else{
-        if(elementos.length > 6 && ((elementos.length * 0.3) < elExclusivos.length)){
+    } else {
+        if (elementos.length > 6 && ((elementos.length * 0.3) < elExclusivos.length)) {
             tipovariavel = "Quantitativa Continua"
-        }else{
+        } else {
             tipovariavel = "Quantitativa Discreta"
         }
     }
-    
+
     celula1.appendChild(document.createTextNode(variavel));
     linha.appendChild(celula1);
     celula2.appendChild(document.createTextNode("Frequência simples"));
@@ -75,8 +75,8 @@ function geraTabela() {
     corpo.appendChild(linha);
 
     elExclusivos.sort(sortFunction);
-    
-    if(tipovariavel == "Quantitativa Discreta" || tipovariavel == "Qualitativa"){
+
+    if (tipovariavel == "Quantitativa Discreta" || tipovariavel == "Qualitativa") {
         for (let elemento in elExclusivos) {
             let linha = document.createElement("tr")
             let celula1 = document.createElement("td");
@@ -87,7 +87,7 @@ function geraTabela() {
             let fsp = parseFloat((ocorrencias[elExclusivos[elemento]] / elementos.length) * 100).toFixed(0);
 
             fac = parseInt(fac) + parseInt(ocorrencias[elExclusivos[elemento]]);
-            facp = ((fac / elementos.length) * 100).toFixed(1) ;
+            facp = ((fac / elementos.length) * 100).toFixed(1);
             celula1.appendChild(document.createTextNode(elExclusivos[elemento]));
             linha.appendChild(celula1);
             celula2.appendChild(document.createTextNode(ocorrencias[elExclusivos[elemento]]));
@@ -101,7 +101,7 @@ function geraTabela() {
             linha.setAttribute("align", "center");
             corpo.appendChild(linha);
         }
-    }else{
+    } else {
         elementos.sort(sortFunction);
         let at = elementos[elementos.length - 1] - elementos[0];
         let k = Math.sqrt(elementos.length).toFixed(0);
@@ -111,15 +111,15 @@ function geraTabela() {
         let elInicial = 0;
         let elFinal = 0;
 
-        while(divisivel == false){
+        while (divisivel == false) {
             at = at + 1;
-            if(at % (k - 1) == 0){
-                classes = k -1;
+            if (at % (k - 1) == 0) {
+                classes = k - 1;
                 divisivel = true;
-            }else if(at % k == 0){
+            } else if (at % k == 0) {
                 classes = k;
                 divisivel = true;
-            }else if(at % (k + 1) == 0){
+            } else if (at % (k + 1) == 0) {
                 classes = k + 1;
                 divisivel = true;
             }
@@ -128,7 +128,7 @@ function geraTabela() {
         intervalo = at / classes;
         elInicial = elementos[0];
 
-        for(let i = 1; i <= classes; i++){
+        for (let i = 1; i <= classes; i++) {
             elFinal = Number(elInicial) + Number(intervalo);
             let qtdElementos = 0;
             let linha = document.createElement("tr")
@@ -138,16 +138,16 @@ function geraTabela() {
             let celula4 = document.createElement("td");
             let celula5 = document.createElement("td");;
 
-            for(let elemento in elementos){
-                if(elementos[elemento] >= elInicial && elementos[elemento] < elFinal){
+            for (let elemento in elementos) {
+                if (elementos[elemento] >= elInicial && elementos[elemento] < elFinal) {
                     qtdElementos = qtdElementos + 1;
                 }
             }
-            
+
             let fsp = parseFloat((qtdElementos / elementos.length) * 100).toFixed(0);
-            
+
             fac = parseInt(fac) + parseInt(qtdElementos);
-            facp = ((fac / elementos.length) * 100).toFixed(1) ;
+            facp = ((fac / elementos.length) * 100).toFixed(1);
             celula1.appendChild(document.createTextNode(elInicial + " |---- " + elFinal));
             linha.appendChild(celula1);
             celula2.appendChild(document.createTextNode(qtdElementos));
@@ -184,10 +184,10 @@ function geraTabela() {
 
     div.innerHTML = "";
 
-    if(tipovariavel == "Qualitativa"){
+    if (tipovariavel == "Qualitativa") {
         celula1.appendChild(document.createTextNode("Média: Não existe"));
         linha.appendChild(celula1);
-    }else{
+    } else {
         celula1.appendChild(document.createTextNode("Média: " + (soma / elementos.length).toFixed(1)));
         linha.appendChild(celula1);
     }
@@ -200,16 +200,16 @@ function geraTabela() {
     div.appendChild(tabela);
     tabela.setAttribute("border", "2");
     tabela.setAttribute("align", "center");
-        
+
     function corAleatoria() {
         var r = Math.floor(Math.random() * 255);
         var g = Math.floor(Math.random() * 255);
         var b = Math.floor(Math.random() * 255);
         return "rgb(" + r + "," + g + "," + b + ")";
-     };
+    };
 
-    if(tipovariavel == "Qualitativa"){
-        for(let elemento in elExclusivos){
+    if (tipovariavel == "Qualitativa") {
+        for (let elemento in elExclusivos) {
             qtd.push(ocorrencias[elExclusivos[elemento]]);
             cor.push(corAleatoria());
         }
@@ -227,8 +227,8 @@ function geraTabela() {
                 }]
             }
         });
-    }else if(tipovariavel == "Quantitativa Discreta"){
-        for(let elemento in elExclusivos){
+    } else if (tipovariavel == "Quantitativa Discreta") {
+        for (let elemento in elExclusivos) {
             qtd.push(ocorrencias[elExclusivos[elemento]]);
             cor.push(corAleatoria());
         }
@@ -248,8 +248,49 @@ function geraTabela() {
             options: {
                 scales: {
                     yAxes: [{
-                        ticks:{
-                            beginAtZero:true
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        });
+    } else {
+        for (let elemento in elExclusivos) {
+            qtd.push(ocorrencias[elExclusivos[elemento]]);
+            cor.push(corAleatoria());
+        }
+
+        let chartGraph = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: elExclusivos,
+                datasets: [{
+                    label: variavel,
+                    data: qtd,
+                    backgroundColor: cor,
+                    borderColor: 'rgba(200, 200, 200, 0.75)',
+                    hoverBorderColor: 'rgba(200, 200, 200, 1)',
+                }]
+            },
+            options: {
+                scales: {
+                    xAxes: [{
+                        display: false,
+                        barPercentage: 1.3,
+                        ticks: {
+                            max: 3,
+                        }
+                    }, {
+                        display: true,
+                        ticks: {
+                            autoSkip: false,
+                            max: 4,
+                        }
+                    }],
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
                         }
                     }]
                 }
