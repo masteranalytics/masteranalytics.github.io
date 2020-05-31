98d48b626f3c4fa4133d41a3b99c132c8145f6a9
Giovani Faleiros <agokdroid@gmail.com>
2020-05-31 16:48:39 -0300

    Update tabela.js

--- js/tabela.js
+++ js/tabela.js
@@ -200,11 +200,15 @@ function geraTabela() {
     if(variavel == "Qualitativa"){
         let rotulos = elExclusivos.toString();
         let chartGraph = new Chart(ctx, {
-            type: 'doughnut',
+            type: 'line',
             data: {
-                labels: ["verde","azul"],
+                labels: ["Jan","Fev","Mar","Abr","Mai","Jun","Jul","Set","Out","Nov","Dez"],
                 datasets: [{
-                    data: [5,7]
+                    label: "Teste 1",
+                    data: [5,10,5,14,20,15,6,14,8,12,15,5,10],
+                    borderWidth: 6,
+                    borderColor: 'rgba(77,166,253,0.85)',
+                    backgroundColor: 'transparent',
                 }]
             }
         })
